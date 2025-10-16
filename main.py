0af903caeb97c8f2af2e2011ffa9c0f3f54e4f8c
pxter <pxter.cs05@nctu.edu.tw>
2025-10-17 00:05:24 +0800

    pointcloud finish?

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ import sys
 
 sys.path.insert(0, os.path.expanduser("~/Third_Party"))
 
+from scipy.spatial.transform import Rotation as R
+from grasp_gen.grasp_server import GraspGenSampler, load_grasp_cfg
 from src.stereo_utils2 import FoundationStereoModel
 from src.yolo_inference import YOLOv5Detector
 from src import (
@@ -23,7 +25,14 @@ from src import (
 )
 from src.zed_utils import ZedCamera
 
-
+zed = None
+###### Models
+stereo_model = None
+sam_predictor = None
+yolo_detector = None
+grasp_cfg = None
+gripper_name = None
+grasp_sampler = None
 def set_logging_format():
     logging.basicConfig(
         level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -86,126 +95,8 @@ def save_json_object_and_scene(
     logging.info(f"Scene saved to {json_filepath}")
 
 
-def save_e57_object_and_scene(
-    out_dir,
-    output_tag,
-    object_points,
-    object_colors,
-    scene_points,
-    scene_colors,
-    timestamp,
-):
-    """Saves the scene points and colors to a .e57 file."""
-    e57_object_filename = f"object_{timestamp}.e57"
-    if output_tag != "":
-        e57_object_filename = f"object_{output_tag}.e57"
-    e57_object_filepath = os.path.join(out_dir, e57_object_filename)
 
-    object_points = np.array(object_points)
-    object_colors = np.array(object_colors)
-
-    object_data = dict()
-    object_data["cartesianX"] = object_points[:, 0]
-    object_data["cartesianY"] = object_points[:, 1]
-    object_data["cartesianZ"] = object_points[:, 2]
-    object_data["colorRed"] = object_colors[:, 2]
-    object_data["colorGreen"] = object_colors[:, 1]
-    object_data["colorBlue"] = object_colors[:, 0]
-
-    with pye57.E57(e57_object_filepath, mode="w") as e57_write:
-        e57_write.write_scan_raw(object_data)
-
-    e57_scene_filename = f"scene_{timestamp}.e57"
-    if output_tag != "":
-        e57_scene_filename = f"scene_{output_tag}.e57"
-    e57_scene_filepath = os.path.join(out_dir, e57_scene_filename)
-
-    scene_points = np.array(scene_points)
-    scene_colors = np.array(scene_colors)
-
-    scene_data = dict()
-    scene_data["cartesianX"] = scene_points[:, 0]
-    scene_data["cartesianY"] = scene_points[:, 1]
-    scene_data["cartesianZ"] = scene_points[:, 2]
-    scene_data["colorRed"] = scene_colors[:, 2]
-    scene_data["colorGreen"] = scene_colors[:, 1]
-    scene_data["colorBlue"] = scene_colors[:, 0]
-
-    with pye57.E57(e57_scene_filepath, mode="w") as e57_write:
-        e57_write.write_scan_raw(scene_data)
-
-    logging.info("Scene saved to e57")
-
-
-def save_mesh(
-    out_dir,
-    object_points,
-    object_colors,
-    captured_vis,
-    timestamp,
-    name,
-    voxel_size=None,
-):
-    """Saves the segmented object as a mesh in an .obj file."""
-    try:
-        segmented_points_np = np.array(object_points)
-        segmented_colors_np = np.array(object_colors)
-
-        if segmented_colors_np.size > 0:
-            segmented_colors_rgb = segmented_colors_np[:, ::-1]
-        else:
-            segmented_colors_rgb = segmented_colors_np
-        segmented_pcd = o3d.geometry.PointCloud()
-        segmented_pcd.points = o3d.utility.Vector3dVector(segmented_points_np)
-        segmented_pcd.colors = o3d.utility.Vector3dVector(segmented_colors_rgb / 255.0)
-
-        if voxel_size:
-            segmented_pcd = segmented_pcd.voxel_down_sample(voxel_size)
-
-        segmented_pcd.estimate_normals(
-            search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.01, max_nn=30)
-        )
-        segmented_pcd.orient_normals_consistent_tangent_plane(k=10)
-
-        dists = segmented_pcd.compute_nearest_neighbor_distance()
-        avg_dist = np.mean(dists)
-        radii = [avg_dist * 2, avg_dist * 3, avg_dist * 5]
-
-        mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
-            segmented_pcd, o3d.utility.DoubleVector(radii)
-        )
-
-        pcd_tree = o3d.geometry.KDTreeFlann(segmented_pcd)
-        mesh_colors = np.asarray(segmented_pcd.colors)[
-            [pcd_tree.search_knn_vector_3d(v, 1)[1][0] for v in mesh.vertices]
-        ]
-        mesh.vertex_colors = o3d.utility.Vector3dVector(mesh_colors)
-
-        mesh_filename = f"segmented_{name}_{timestamp}.obj"
-        mesh_filepath = os.path.join(out_dir, mesh_filename)
-
-        o3d.io.write_triangle_mesh(mesh_filepath, mesh, write_vertex_colors=True)
-        logging.info(f"Reconstructed mesh saved to {mesh_filepath}")
-
-        vis_filename = f"segmented_vis_{timestamp}.png"
-        vis_filepath = os.path.join(out_dir, vis_filename)
-        cv2.imwrite(vis_filepath, captured_vis)
-        logging.info(f"Combined visualization saved to {vis_filepath}")
-
-    except Exception as e:
-        logging.error(f"An error occurred during mesh reconstruction or saving: {e}")
-
-
-def save_capture_view(captured_vis, out_dir, output_tag, timestamp):
-    vis_filename = f"segmented_vis_{timestamp}.png"
-    if output_tag != "":
-        vis_filename = f"segmented_vis_{output_tag}.png"
-    vis_filepath = os.path.join(out_dir, vis_filename)
-    cv2.imwrite(vis_filepath, captured_vis)
-    logging.info(f"Combined visualization saved to {vis_filepath}")
-
-
-def save_zed_point_cloud(
+def generate_zed_point_cloud(
     args,
     K_cam,
     depth,
@@ -278,55 +169,31 @@ def save_zed_point_cloud(
         logging.warning(
             "The selected mask contains no points from the point cloud. Nothing to save."
         )
-        return
-
     scene_points = [[z, -x, -y] for x, y, z in scene_points]
     object_points = [[z, -x, -y] for x, y, z in object_points]
 
-    current_time_str = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    """Saves the scene and object data to a JSON file."""
+    object_colors_arr = np.array(object_colors)
+    if object_colors_arr.size > 0:
+        object_colors_arr = object_colors_arr[:, ::-1]
 
-    save_json_object_and_scene(
-        args.out_dir,
-        args.output_tag,
-        object_points,
-        object_colors,
-        scene_points,
-        scene_colors,
-        current_time_str,
-    )
-    save_capture_view(captured_vis, args.out_dir, args.output_tag, current_time_str)
-    if args.save_e57:
-        save_e57_object_and_scene(
-            args.out_dir,
-            args.output_tag,
-            object_points,
-            object_colors,
-            scene_points,
-            scene_colors,
-            current_time_str,
-        )
-    if args.exit_after_save:
-        print("saved and finish")
-        exit(0)
-    """
-    save_mesh(
-        args.out_dir,
-        object_points,
-        object_colors,
-        captured_vis,
-        current_time_str,
-        "object",
-    )
-    save_mesh(
-        args.out_dir,
-        scene_points,
-        scene_colors,
-        captured_vis,
-        current_time_str,
-        "scene",
-        voxel_size=0.01,
-    )
-    """
+    scene_colors_arr = np.array(scene_colors)
+    if scene_colors_arr.size > 0:
+        scene_colors_arr = scene_colors_arr[:, ::-1]
+
+    scene_data = {
+        "object_info": {
+            "pc": np.array(object_points),
+            "pc_color": object_colors_arr,
+        },
+        "scene_info": {
+            "pc_color": [np.array(scene_points)],
+            "img_color": [scene_colors_arr],
+        },
+        "grasp_info": {"grasp_poses": [], "grasp_conf": []},
+    }
+    return scene_data
+
 
 
 def parse_args():
@@ -384,120 +251,188 @@ def parse_args():
     parser.add_argument(
         "--max-depth",
         type=float,
-        default=None,
+        default=3,
         help="Maximum depth in meters.",
+    )
+    ### GraspGen
+    parser.add_argument(
+        "--sample_data_dir",
+        type=str,
+        default=str(config.GRASPGEN_SCENE_DIR),
+        help="Directory containing JSON files with point cloud data",
+    )
+    parser.add_argument(
+        "--gripper_config",
+        type=str,
+        default=str(config.GRIPPER_CFG),
+        help="Path to gripper configuration YAML file",
+    )
+    parser.add_argument(
+        "--grasp_threshold",
+        type=float,
+        default=0.70,
+        help="Threshold for valid grasps. If -1.0, then the top 100 grasps will be ranked and returned",
+    )
+    parser.add_argument(
+        "--num_grasps",
+        type=int,
+        default=200,
+        help="Number of grasps to generate",
+    )
+    parser.add_argument(
+        "--return_topk",
+        action="store_true",
+        help="Whether to return only the top k grasps",
+    )
+    parser.add_argument(
+        "--topk_num_grasps",
+        type=int,
+        default=5,
+        help="Number of top grasps to return when return_topk is True",
+    )
+    parser.add_argument(
+        "--transform-config",
+        type=str,
+        default="demo4.json",
+        help="Transform config",
     )
     return parser.parse_args()
 
 
+def gen_point_cloud(args):
+    
+    try:
+        set_seed(0)
+        torch.autograd.set_grad_enabled(False)
+        zed_status, left_image, right_image = zed.capture_images()
+        left_gray = cv2.cvtColor(left_image.get_data(), cv2.COLOR_BGRA2GRAY)
+        right_gray = cv2.cvtColor(right_image.get_data(), cv2.COLOR_BGRA2GRAY)
+        color_np = left_image.get_data()[:, :, :3]  # Drop alpha channel
+        color_np_org = color_np.copy()
+        mask = np.zeros_like(color_np[:, :, 0], dtype=bool)
+
+        df = yolo_detector.infer(color_np_org)
+
+        cup_detections = df[df["name"] == "cup"]
+        if cup_detections.empty:
+            print("No Cup")
+            raise ValueError
+        cup_box = cup_detections.iloc[0]
+        box = (
+            int(cup_box["xmin"]),
+            int(cup_box["ymin"]),
+            int(cup_box["xmax"]),
+            int(cup_box["ymax"]),
+        )
+        # mask
+        color_np_rgb = cv2.cvtColor(color_np, cv2.COLOR_BGR2RGB)
+        mask = sam_utils.run_sam2(
+            sam_predictor,
+            color_np_rgb,
+            box,
+            iterations=args.erosion_iterations,
+        )
+        # zed inference
+        depth, (H_scaled, W_scaled) = stereo_model.run_inference(
+            left_gray, right_gray, zed.K_left, zed.baseline
+        )
+        # gen pointcloud
+        return generate_zed_point_cloud(
+            args,
+            zed.K_left,
+            depth,
+            color_np_org,
+            mask,
+            color_np_org,
+        )
+
+
+    except:
+        print("failed during pointcloud")
+        return None
+
+def transform(original_pointcloud, transformation_matrix):
+    original_pc_homogeneous = np.hstack(
+        (
+            original_pointcloud,
+            np.ones((original_pointcloud.shape[0], 1)),
+        )
+    )
+    transformed_object_pc_homogeneous = (
+        transformation_matrix @ original_pc_homogeneous.T
+    ).T
+    transformed_pointcloud = transformed_object_pc_homogeneous[:, :3]
+    return transformed_pointcloud
+
+def quick_transform(original_pointcloud, transform_config):
+    # check and load transform config
+    if transform_config == "":
+        print("Please provide transform config")
+        exit(1)
+    transform_filename = os.path.join("transform_config", transform_config)
+    with open(transform_filename, "rb") as f:
+        transform_data = json.load(f)
+
+    # Build Transformation matrix
+    translation_matrix = np.array(
+        [
+            [1, 0, 0, transform_data["tx"]],
+            [0, 1, 0, transform_data["ty"]],
+            [0, 0, 1, transform_data["tz"]],
+            [0, 0, 0, 1],
+        ]
+    )
+    rotation = R.from_euler(
+        "xyz",
+        [transform_data["rr"], transform_data["rp"], transform_data["ry"]],
+        degrees=True,
+    )
+    rotation_matrix = np.identity(4)
+    rotation_matrix[:3, :3] = rotation.as_matrix()
+    transformation = translation_matrix @ rotation_matrix
+
+    # check and load pointcloud
+
+    info = original_pointcloud
+    info["object_info"]["pc"] = transform(info["object_info"]["pc"], transformation)
+    info["scene_info"]["pc_color"] = [transform(np.array(info["scene_info"]["pc_color"][0]) , transformation)]
+
+
+
+    return info
+
+
 def main():
     args = parse_args()
-
-    set_logging_format()
-    set_seed(0)
-    torch.autograd.set_grad_enabled(False)
-    os.makedirs(args.out_dir, exist_ok=True)
-
-    # ---------- Load Models ----------
+    global zed, stereo_model, sam_predictor, yolo_detector, grasp_cfg, gripper_name, grasp_sampler
+    zed = ZedCamera()
     stereo_model = FoundationStereoModel(args)
     sam_predictor = sam_utils.load_sam_model()
-    if args.use_yolo:
-        yolo_detector = YOLOv5Detector(model_path="/home/j300/models/YOLOModels/yolov5x.pt",conf=0.4)
+    yolo_detector = YOLOv5Detector(model_path="/home/j300/models/YOLOModels/yolov5x.pt",conf=0.4)
 
-    # ---------- ZED Init ----------
-    zed = ZedCamera()
+    grasp_cfg = load_grasp_cfg(args.gripper_config)
+    gripper_name = grasp_cfg.data.gripper_name
+    grasp_sampler = GraspGenSampler(grasp_cfg)
 
-    # ---------- Window and Mouse Callback Setup ----------
-    win_name = "RGB + Mask | Depth"
-    cv2.namedWindow(win_name)
-    cv2.setMouseCallback(win_name, mouse_handler.select_box)
-    logging.info("Streaming... Draw a box with your mouse.")
-    logging.info("Press SPACE to save, 'r' to reset box, ESC to exit.")
     try:
         while True:
-            zed_status, left_image, right_image = zed.capture_images()
-            if zed_status == sl.ERROR_CODE.SUCCESS:
-                # Convert to numpy arrays
-                left_gray = cv2.cvtColor(left_image.get_data(), cv2.COLOR_BGRA2GRAY)
-                right_gray = cv2.cvtColor(right_image.get_data(), cv2.COLOR_BGRA2GRAY)
-                color_np = left_image.get_data()[:, :, :3]  # Drop alpha channel
-                color_np_org = color_np.copy()
-
-                display_frame = color_np.copy()
-
-                # ---------- Manual Box Selection + SAM2 Logic ----------
-                mask = np.zeros_like(color_np[:, :, 0], dtype=bool)
-                
-                key = cv2.waitKey(1)
-                if key == ord("y") and args.use_yolo:
-                    df = yolo_detector.infer(display_frame)
-                    if "name" in df.columns:
-                        cup_detections = df[df["name"] == "cup"]
-                        if not cup_detections.empty:
-                            logging.info("Cup detected, selecting it.")
-                            cup_box = cup_detections.iloc[0]
-                            box = (
-                                int(cup_box["xmin"]),
-                                int(cup_box["ymin"]),
-                                int(cup_box["xmax"]),
-                                int(cup_box["ymax"]),
-                            )
-                            mouse_handler.box_start_point = (box[0], box[1])
-                            mouse_handler.box_end_point = (box[2], box[3])
-                            mouse_handler.box_defined = True
-                            mouse_handler.drawing_box = False
-
-                if mouse_handler.drawing_box:
-                    visualization.draw_box(
-                        display_frame,
-                        mouse_handler.box_start_point,
-                        mouse_handler.box_end_point,
-                    )
-
-                if mouse_handler.box_defined:
-                    box = mouse_handler.get_box()
-                    visualization.draw_box(
-                        display_frame, (box[0], box[1]), (box[2], box[3])
-                    )
-
-                    color_np_rgb = cv2.cvtColor(color_np, cv2.COLOR_BGR2RGB)
-                    mask = sam_utils.run_sam2(
-                        sam_predictor,
-                        color_np_rgb,
-                        box,
-                        iterations=args.erosion_iterations,
-                    )
-                    display_frame = visualization.overlay_mask_on_frame(
-                        display_frame, mask
-                    )
-
-                # ---------- FoundationStereo Inference ----------
-
-                cv2.imshow(win_name, display_frame)
-                
-                if key == ord("r"):
-                    logging.info("Box reset. Draw a new one.")
-                    mouse_handler.reset_box()
-
-                if key == 32 and mouse_handler.box_defined:
-                    depth, (H_scaled, W_scaled) = stereo_model.run_inference(
-                        left_gray, right_gray, zed.K_left, zed.baseline
-                    )
-                    save_zed_point_cloud(
-                        args,
-                        zed.K_left,
-                        depth,
-                        color_np_org,
-                        mask,
-                        display_frame,
-                    )
-
-                if key == 27:
-                    break
+            text = input("text start to start")
+            if text == "start":
+                pointcloud = gen_point_cloud(args)
+                if pointcloud is None:
+                    continue
+                transform_pointcloud = quick_transform(pointcloud, args.transform_config)
+                print("what?")
+            elif text == "end":
+                break
+            
+    except KeyboardInterrupt:
+        zed.close()
     finally:
         zed.close()
-        cv2.destroyAllWindows()
 
+
+    
 
 if __name__ == "__main__":
     main()
