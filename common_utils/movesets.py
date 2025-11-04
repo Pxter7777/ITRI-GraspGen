6ced0bb643d8e5ee6365b31c82c1347c06376b6b
pxter <pxter.cs05@nctu.edu.tw>
2025-11-04 21:21:20 +0800

    fix movesets error mass_center and std format

--- common_utils/movesets.py
+++ common_utils/movesets.py
@@ -116,10 +116,10 @@ def grab_and_pour_and_place_back_curobo(
         if target_name != obj_name:
             obstacles.append(
                 {
-                    "mass_center": np.mean(
-                        scene_data["object_infos"][obj_name]["points"]
-                    ),
-                    "std": np.std(scene_data["object_infos"][obj_name]["points"]),
+                    "mass_center": list(np.mean(
+                        scene_data["object_infos"][obj_name]["points"], axis=0
+                    )),
+                    "std": list(np.std(scene_data["object_infos"][obj_name]["points"], axis=0)),
                 }
             )
     moves = []
@@ -265,8 +265,8 @@ def move_to_curobo(
     for obj_name in scene_data["object_infos"]:
         obstacles.append(
             {
-                "mass_center": np.mean(scene_data["object_infos"][obj_name]["points"]),
-                "std": np.std(scene_data["object_infos"][obj_name]["points"]),
+                "mass_center": list(np.mean(scene_data["object_infos"][obj_name]["points"], axis=0)),
+                "std": list(np.std(scene_data["object_infos"][obj_name]["points"], axis=0)),
             }
         )
     moves = []
