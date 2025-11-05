69726be0d8fc211d2f97f8af13c277eca653d1c9
pxter <pxter.cs05@nctu.edu.tw>
2025-11-05 10:16:33 +0800

    modify simple_send to easily test isaacsim

--- test_tool_for_isaacsim/simple_send.py
+++ test_tool_for_isaacsim/simple_send.py
@@ -6,6 +6,9 @@ from common_utils.socket_communication import (
 )
 import json
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 sender = NonBlockingJSONSender(port=9878)
 receiever = BlockingJSONReceiver(port=9879)
@@ -13,11 +16,28 @@ try:
     current_file_dir = os.path.dirname(os.path.abspath(__file__))
     project_root_dir = os.path.dirname(current_file_dir)
     filepath = os.path.join(
-        project_root_dir, "data_for_test", "fullact", "green_cup_to_pan.json"
+        project_root_dir, "data_for_test", "fullact", "fullact_20251104_201025.json"
     )
     with open(filepath, "rb") as f:
-        data = json.load(f)
-    sender.send_data(data)
+        data1 = json.load(f)
+    
+    filepath = os.path.join(
+        project_root_dir, "data_for_test", "fullact", "fullact_20251104_201027.json"
+    )
+    with open(filepath, "rb") as f:
+        data2 = json.load(f)
+
+    filepath = os.path.join(
+        project_root_dir, "data_for_test", "fullact", "fullact20251104_201051.json"
+    )
+    with open(filepath, "rb") as f:
+        data3 = json.load(f)
+
+    datas = [data1, data2, data3]
+
+    for data in datas:
+        sender.send_data(data)
+        message = receiever.capture_data()
 finally:
     sender.disconnect()
     receiever.disconnect()
