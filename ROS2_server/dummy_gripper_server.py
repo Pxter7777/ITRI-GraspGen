5a26448c292432d62dbdd6338c0b787435dc2c54
pxter <pxter.cs05@nctu.edu.tw>
2025-11-28 15:59:45 +0800

    make dummy server faster

--- ROS2_server/dummy_gripper_server.py
+++ ROS2_server/dummy_gripper_server.py
@@ -38,7 +38,7 @@ while True:
     logger.info("Received data!")
     try:
         if data["type"] == "arm":
-            time.sleep(2.0)
+            time.sleep(0.5)
         time.sleep(data["wait_time"])
     except KeyError as e:
         logger.exception(
